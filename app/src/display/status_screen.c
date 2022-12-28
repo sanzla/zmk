423081826205dd28af739b15ba63b2479723ea26
Luis Sanchez <sanzluisangel@icloud.com>
2022-12-28 00:17:57 -0600

    rotate by 90 wpm

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -68,6 +68,7 @@ lv_obj_t *zmk_display_status_screen() {
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_disp_set_rotation(NULL, 1);
     lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, 0,
                  0);
 #endif
