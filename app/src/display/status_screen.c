59a995e8c96879f915949a5aa4662941150ebb95
Luis Sanchez <sanzluisangel@icloud.com>
2022-12-28 00:48:12 -0600

    revert

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -38,7 +38,6 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
 
     screen = lv_obj_create(NULL, NULL);
-    screen->driver->rotated = 1;
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
