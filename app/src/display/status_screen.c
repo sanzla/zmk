cf42badb0176879f8a0a91edfdbfa6e66275f806
LUIS SANCHEZ <sanzluisangel@icloud.com>
2023-01-07 13:24:30 -0600

    move battery to the left

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -42,7 +42,7 @@ lv_obj_t *zmk_display_status_screen() {
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_BATTERY_STATUS)
     zmk_widget_battery_status_init(&battery_status_widget, screen);
     lv_obj_align(zmk_widget_battery_status_obj(&battery_status_widget), NULL, LV_ALIGN_IN_TOP_RIGHT,
-                 10, 10);
+                 -10, 0);
 #endif
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_OUTPUT_STATUS)
