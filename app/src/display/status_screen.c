daa2f43ffbd2982eada4f8a881b1ff219b319f76
LUIS SANCHEZ <sanzluisangel@icloud.com>
2023-01-09 12:54:58 -0600

    mod display on the bottom left

--- app/src/display/status_screen.c
+++ app/src/display/status_screen.c
@@ -9,6 +9,7 @@
 #include <zmk/display/widgets/battery_status.h>
 #include <zmk/display/widgets/layer_status.h>
 #include <zmk/display/widgets/wpm_status.h>
+#incluce <zmk/display/widgets/mods_status.h>
 #include <zmk/display/status_screen.h>
 
 #include <logging/log.h>
@@ -32,6 +33,10 @@ static struct zmk_widget_layer_status layer_status_widget;
 
 #if IS_ENABLED(CONFIG_ZMK_WIDGET_WPM_STATUS)
 static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_MODS_STATUS)
+static struct zmk_widget_mods_status mods_status_widget;
 #endif
 
 lv_obj_t *zmk_display_status_screen() {
@@ -71,5 +76,12 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, 0,
                  -10);
 #endif
+
+#if IS_ENABLED(CONFIG_ZMK_WIDGET_MODS_STATUS)
+    zmk_widget_mods_status_init(&mods_status_widget, screen);
+    lv_obj_align(zmk_widget_mods_status_obj(&mods_status_widget), NULL, LV_ALIGN_IN_BOTTOM_LEFT, 0,
+                 0);
+#endif
+
     return screen;
 }
